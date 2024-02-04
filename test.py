fa5864d1604570ac56c6b25d8338f1b4411ad5da
mkumawat <kumawat.manish1307@gmail.com>
2024-02-04 11:31:06 +0530

    forth commit

--- test.py
+++ test.py
@@ -3,7 +3,8 @@ import pandas as pd
 data = [
     {"name":"manish", "age":29,"city":"sikar"},
     {"name":"krish", "age":35,"city":"bangaluru"},
-    {"name":"sunny", "age":27,"city":"bhopal"}
+    {"name":"sunny", "age":27,"city":"bhopal"},
+    {"name":"sudh", "age":37,"city":"delhi"}
 ]
 
 df=pd.DataFrame(data)
